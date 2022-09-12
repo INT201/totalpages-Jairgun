b56bd3da9461adf38e9885a90a592ef33b23e3b1
Jairgun <jasda.jair9un@mail.kmutt.ac.th>
2022-09-12 10:45:18 +0700

    test

--- totalPages.js
+++ totalPages.js
@@ -3,8 +3,8 @@ const { template } = require('@babel/core')
 function totalPages(arrayItems, rowsPerPage) {
   //code here
   page = arrayItems.length/rowsPerPage
-  if(totalPages?.arrayItems == undefined) return 1
-  else if(totalPages?.rowsPerPage == undefined) return undefined
+  if(arrayItems == undefined || null) return 1
+  else if(rowsPerPage == undefined || null) return undefined
   else return Math.ceil(page)
 }
 module.exports = totalPages
