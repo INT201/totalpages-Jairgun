9bc6b1e21d12a0bdf7e4f49d44ddd0a525829ab8
Jairgun <jasda.jair9un@mail.kmutt.ac.th>
2022-09-12 10:51:00 +0700

    test

--- totalPages.js
+++ totalPages.js
@@ -2,10 +2,17 @@ const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
   //code here
-if(rowsPerPage == null || undefined){console.log(1)}
-else if(arrayItems == null || undefined){console.log(undefined)}
-else{page = arrayItems.length/rowsPerPage    
-      if(page = Infinity){return 1}
+  if(rowsPerPage == null || undefined){
+    console.log(1)
+}
+else if(arrayItems == null || undefined){
+    console.log(undefined)
+}
+else{
+    page = arrayItems.length/rowsPerPage
+    if(page = Infinity){
+        return 1
+    }
     console.log(Math.ceil(page))
 }
 }
